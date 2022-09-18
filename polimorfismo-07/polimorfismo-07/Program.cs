5700271ec06af1cad89f82853faafe1c4ce12a3f
Aloe <heros.dasilva@gmail.com>
2022-09-18 17:48:05 -0300

    Entrega lista 7

---
+++ polimorfismo-07/polimorfismo-07/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace polimorfismo_07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
